d632fa7a2a27f5b137e44dd893329d524eeeaf11
PacSocks <75374437+PacSocks@users.noreply.github.com>
2021-07-11 16:58:58 +0800

    Added customizable default

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const Discord = require("discord.js"),
-  {Structures} = Discord;
+const {Structures} = require("discord.js);
 
 // loads discord-reply, this wraps that...
 require("discord-reply");
+
+var mention;
 
 class PatchedMessage extends Structures.get("Message") {
 
   constructor() {
     super(...arguments);
   }
-
-  reply(content, options) {
-    // default = true
-    if (options && options.allowedMentions && options.allowedMentions.repliedUser === false) {
-      return this.lineReplyNoMention(content, options);
-    } else {
-      return this.lineReply(content, options);
+  
+  reply(...args) {
+    if(args[0]===false){
+      args.shift();
+      return lineReplyNoMention(...args);
+    }else{
+      if(mention===false&&args[0]!==true)return lineReplyNoMention(...args);
+      return lineReply(...args);
     }
   }
 
 }
 
 Structures.extend("Message", () => PatchedMessage);
+
+module.exports = function(bool){
+  if(!typeof bool === "boolean")throw new TypeError(`Expected boolean but got ${typeof bool} instead`);
+  mention = bool;
+}
